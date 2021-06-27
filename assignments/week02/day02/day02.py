8425923a43df1c079593d651e15034fb40c6bb2d
reachjitendra27@gmail.com <85295592+Jitendra-gautam-au19@users.noreply.github.com>
2021-06-27 12:26:25 +0530

    week02/day02

---
+++ assignments/week02/day02/day02.py
@@ -0,0 +1,8 @@
+#Ask user for basic info like (name, mobile, age etc) one by one
+#Display the information in a clean format
+#Display Data Type of each information in another line
+age = input("Enter age: ")
+name = input("Enter name: ")
+mobile = input("Enter mobile no.: ")
+organisation =input("Enter Organisation name: " )
+print("Hi, my name is " + name + ", and I am " + age + " years old." + mobile +"mobile no." + organisation + "Organisation name")
